df2323bbddee1232b32981807cb741095b6dc11b
Bebido <Maniek4182@interia.pl>
2016-09-29 19:18:29 +0200

    test

--- SBD_01/Source.cpp
+++ SBD_01/Source.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	return 0;///
+	char wybor;
+	wybor = getchar();
+
+	switch (wybor)
+	{
+	case 'l':
+		cout << "los\n";
+		break;
+	case 'm':
+		cout << "manual\n";
+		break;
+	default:
+		cout << "plik\n";
+		break;
+	}
+
+	system("pause");
 }
