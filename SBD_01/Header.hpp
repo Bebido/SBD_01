22b3778702c4fc1f0c9909b711496bf506ff7dfa
Bebido <Maniek4182@interia.pl>
2016-10-11 13:11:20 +0200

    podzial

---
+++ SBD_01/Header.hpp
@@ -0,0 +1,8 @@
+#ifndef header_cpp
+#define header_cpp
+#include <iostream>
+
+void wybor();
+
+#endif // !header_cpp
+
--- SBD_01/Source.cpp
+++ SBD_01/Source.cpp
@@ -1,32 +1,10 @@
-#include <iostream>
-using namespace std;
-void wybor();
+#include "Header.hpp"
+
+using std::cin;
+using std::cout;
 
 int main()
 {
 	wybor();
 	system("pause");
-}
-
-void wybor()
-{
-	char wybor;
-	cout << "Wybierz metoda wprowadzania:\n m - manualnie\n l - losowo\n f - z pliku\n";
-	wybor = getchar();
-
-	switch (wybor)
-	{
-	case 'l':
-		cout << "los\n";
-		break;
-	case 'm':
-		cout << "manual\n";
-		break;
-	case 'f':
-		cout << "plik\n";
-		break;
-	default:
-		cout << "nada\n";
-		break;
-	}
 }
